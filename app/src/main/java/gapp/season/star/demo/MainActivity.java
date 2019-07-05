a06bca364e523fd002022b61862e061835f0ce77
guang <guang.gg2018@gmail.com>
2019-07-05 14:46:27 +0800

    add demo project

---
+++ app/src/main/java/gapp/season/star/demo/MainActivity.java
@@ -0,0 +1,33 @@
+package gapp.season.star.demo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+                return;
+            }
+        }
+
+        showStarrySky(null);
+    }
+
+    public void showStarrySky(View view) {
+    }
+}
